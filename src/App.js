f6c87863a493bc9708337c31a64c1f38a6efea58
m-gamao <42097750+m-gamao@users.noreply.github.com>
2024-10-08 17:23:25 -0400

    updated App.js to include Medication details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DiagnosisSearch from "./components/DiagnosisSearch";
-import MedicationList from "./components/MedicationList"; // Medication List screen (we'll create it next)
+import MedicationList from "./components/MedicationList";
+import MedicationDetails from "./components/MedicationDetails"; // New component
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DiagnosisSearch />} />
           <Route path="/medications/:diagnosis" element={<MedicationList />} />
+          <Route path="/medications/:diagnosis/:medication" element={<MedicationDetails />} /> {/* New route */}
         </Routes>
       </div>
     </Router>
